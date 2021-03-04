9bd82f85900c99a2502d20a533e9302848533594
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2021-03-04 10:59:33 -0300

    Configurations file added to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 if [ ! -d  "logs" ]; then mkdir logs; touch logs/error.log; fi
 if [ -a "/etc/apache2/sites-available/lpgp-api.conf" ]; then
     echo -e "Seems that there's a configurations file installed already.\nSo you don't have to install it again"
-    exit 0
 else
     # installation begins
     # make sure the script was started by the root user
@@ -13,5 +12,21 @@ else
     ssl_key_location=""
     ssl_certificate_location=""
     echo -e "First of all enter the document root of the project: "; read document_root
+    # TODO
+fi
 
+if [ -a "config/config.xml" ]; then
+	echo -e "Seems you don't have to generate a configurations file too, awesome!"
+else
+	touch config/config.xml
+	XML_STRC="""
+<?xml version="1.0" encoding=\"UTF-8\"?>
+<api_config>
+	<ext_config value=\"\"/>
+	<gen_logs_path value=\"\"/>
+	<error_log_path value=\"\"/>
+</api_config>
+	"""
+	echo -en $XML_STRC | tee config/config.xml
+	echo "Configurations file done, put your content there now"
 fi
