31817ef56f8e2effaab8679a1767f23d42ed4e27
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2021-03-07 12:46:17 -0300

    Still building

--- classes/Client.php
+++ classes/Client.php
@@ -2,8 +2,10 @@
 namespace Clients;
 require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/exceptions/clients.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/clients-data.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/classes/clients-access-data.php";
 
 use Core\ClientsData;
+use Core\ClientsAccessData;
 
 /**
  * <Class> Represents the client logged by the token
@@ -15,7 +17,7 @@ use Core\ClientsData;
  * @var string|null $token The logged client token
  * @var integer|null $client The client primary key reference from the database
  * @var boolean|null $rootClient If the logged client have root permissions
- * @var 
+ * @var
  */
 class Client{
 
